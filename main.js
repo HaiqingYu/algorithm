653292acc90eae97542016ed2ac3fd9da26047f6
haiqingYu <yu.haiqing@oyohotels.cn>
2019-10-23 20:22:50 +0800

    ;

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 /**
  * 求两个数的最大公约是
  */
-function foo(n1, n2) {
+function GCD(n1, n2) {
   return n1 * n2;
 }
 
-function test(n1, n2) {
-  var result = n1 + ' and ' + n2 + ' equals: ' + foo(n1, n2) + '           ';
-  document.getElementById('main').innerHTML += result;
+function LCM(n1, n2) {
+  return 1;
 }
 
-test(400, 800);
-test(5, 19);
+function test() {
+  var dom = document.createElement('div');
+  dom.innerHTML = 'GCD 400 and 800 equals: ' + GCD(400, 800);
+  document.getElementById('main').appendChild(dom);
+  dom = document.createElement('div');
+  dom.innerHTML = 'GCD 25 and 35 equals: ' + GCD(25, 35);
+  document.getElementById('main').appendChild(dom);
+  dom = document.createElement('div');
+  dom.innerHTML = 'LCM 25 and 35 equals: ' + LCM(25, 35);
+  document.getElementById('main').appendChild(dom);
+}
+test();
